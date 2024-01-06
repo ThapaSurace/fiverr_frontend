fabf9592f6ab8af8bcf9902189f69fe8a5fc16c7
Suresh Thapa <zorosenpai456@gmail.com>
2024-01-06 17:56:28 +0545

    url added

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL:"https://fiverrapi.up.railway.app/api/",
+    baseURL:"https://api-fiverr-5egd.onrender.com//api/",
     withCredentials: true
 })
 
